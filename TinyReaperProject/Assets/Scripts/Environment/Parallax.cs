31546b2edb9f6d3d09e1a2272d767c63cdfa846d
JohnSausage <juliangreindl88@gmail.com>
2022-04-23 10:09:46 +0200

    worked on level and parallax

---
+++ TinyReaperProject/Assets/Scripts/Environment/Parallax.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxScaleX = 0.1f;
+    [SerializeField] private float parallaxScaleY = 0.1f;
+
+    private Transform _parallaxCamera;
+    private Vector2 _startPos;
+    private Vector2 _currentCameraPos;
+    private Vector2 _localStartPos;
+
+    void Start()
+    {
+        _parallaxCamera = Camera.main.transform;
+
+        Debug.Log(transform.localPosition);
+        _startPos = GetComponentInChildren<Renderer>().bounds.center;
+        _localStartPos = transform.localPosition;
+    }
+
+    void FixedUpdate()
+    {
+        _currentCameraPos = (Vector2)_parallaxCamera.position - _startPos;
+
+        transform.localPosition = new Vector3(_currentCameraPos.x * parallaxScaleX, _currentCameraPos.y * parallaxScaleY, transform.localPosition.z);
+        transform.localPosition += (Vector3)_localStartPos;
+    }
+}
